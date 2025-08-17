a3ceee4a8a0927def66274dbfc80979a26f21fd2
Rhovic Ataiza <devrhovic@gmail.com>
2025-08-17 11:24:20 +0800

    added a card for the weather forecast

--- lib/weather_screen.dart
+++ lib/weather_screen.dart
@@ -68,6 +68,34 @@ class WeatherScreen extends StatelessWidget {
               'Weather Forecast',
               style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
             ),
+            const SizedBox(height: 14),
+            SingleChildScrollView(scrollDirection: Axis.horizontal),
+            Row(
+              children: [
+                Card(
+                  elevation: 6,
+                  child: Container(
+                    width: 100,
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      children: [
+                        Text(
+                          '09:00',
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.w800,
+                          ),
+                        ),
+                        const SizedBox(height: 6),
+                        Icon(Icons.cloud, size: 40),
+                        const SizedBox(height: 6),
+                        Text('301.00', style: TextStyle(fontSize: 14)),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
             //additional information
             const SizedBox(height: 20),
             const Placeholder(fallbackHeight: 150),
